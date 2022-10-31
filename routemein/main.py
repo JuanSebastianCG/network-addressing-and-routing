c2634652ae2a8d2758244d74c1950db1b646f1b7
Juan cardenas <juans.cardenasg@autonoma.edu.co>
2022-10-31 18:49:27 -0500

    platillas para generar configuraciones basicas

--- routemein/main.py
+++ routemein/main.py
@@ -38,77 +38,73 @@ from Library.conections.RmiWanConection import WanConection as WanConection
 """dhcpEasyIp(name: String, initialIp: ip, [[exclusiveip,fromIpHowMani]], dns: Ip, gateWay)  """
 easyIp = [
     dhcpEasyIp("R1Fa0",
-               ip(150,30,4,0,25),
-               [[ip( 150,30,4,1,25),9]],
-               ip(150,30,0,15,30),
-               ip(150,30,4,1,25))
+               ip(172,30,0,0,21),
+               [[ip(172,30,0,1,21),9]],
+               ip(172,30,8,66,27),
+               ip(172,30,0,1,21))
     ,
-    dhcpEasyIp("R1Fa1",
-               ip(150,30,4,128,27),
-               [[ip(150,30,4,129,27),9]],
-               ip(150,30,0,15,30),
-               ip(150,30,4,129,27)),
+      dhcpEasyIp("R1Fa1",
+               ip(172,30,8,0,21),
+               [[ip(172,30,8,1,21),9]],
+               ip(172,30,8,66,27),
+               ip(172,30,8,1,21))
 ]
 
+
+""" ip, gateway """
 host1 = [easyIp[0].getNextIp(),easyIp[0].gateWay]
 host2 = [easyIp[1].getNextIp(),easyIp[1].gateWay]
-server1 = [ip(150,30,0,2,22), ip(150,30,0,15,22)]
-server2 = [ip(192,168,10,2,24),ip(192,168,10,1,24)]
+
+server1 = [ip(172,30,8,65,27), ip(172,30,8,66,27)]
+server2 = [ip(200,20,10,129,25),ip(200,20,10,130,25)]
 
 
-ipStaticas = [ip(210,20,0,190)]
-ipPublicas = [ip(210,20,0,161), ip(210,20,0,172)]
+ipStaticas = [ip(223,120,12,21,28)]
+ipPublicas = [ip(223,120,12,21,28), ip(223,120,12,27,28)]
 
+
+helper = dhcpHelper(ip(192,168,80,2,29))
 
 """ Nat(Name, [publicIp,publicIp,publicIp], maskPublicIp, ip afected by the dinamic Nat ,[[staticIpinternal, staticIpExternal],.. ], ...) """
 nat =  Nat("MY-NAT-POOL", 
-           ipPublicas,ip.getIpMaskFromMask(28),
-           [easyIp[0].initialIp, easyIp[1].initialIp],
-           [[server1[0],ipStaticas[0]] ]
+           ipPublicas,ip(255,255,255,240),
+           [easyIp[0].initialIp, easyIp[1].initialIp, ip(172,30,8,64,27)],
+           [[server1[1],ipStaticas[0]] ]
            )
 
-""" rd(name of the device, setting [dhcpEasyIp,setting2](opcional) ,serial port ejem: [port("0/0"),port("0/2")](opcional), fasethernet port ejem:  [port("0/0"),port("0/2")](opcional)  ) """
+""" rd(name of the device, setting [dhcpEasyIp,setting2,....](opcional) ,serial port ejem: [port("0/0"),port("0/2")](opcional), fasethernet port ejem:  [port("0/0"),port("0/2")](opcional)  ) """
 routers = [
-           RouterD("router0",[easyIp[0], easyIp[1]]),
-           RouterD("router1",[nat]),
-           RouterD("router2"),
+           RouterD("londres",[easyIp[0], easyIp[1], nat]),
+           RouterD("paris",[helper]),
            RouterD("ISP"),
         ]
 
 """ hd (name, assigned ip, gateWay : ip(opcional), fasethernet port :  [port("0/0"),port("0/2")] (opcional))"""
-hosts = [HostD("pc0",  host1[0],host1[1]),
+hosts = [
+         HostD("pc0",  host1[0],host1[1]),
          HostD("pc1", host2[0], host2[1]),
-         HostD("brasil",server1[0],server1[1]) ,
-         HostD("server1",server2[1],server2[1]) 
+         HostD("londres",server1[0],server1[1]) ,
+         HostD("serverDns",server2[0],server2[1]) 
           ]
-
-
 
 """ switch (name, vlans: vlan, assigned ip, gateWay : ip(opcional), fasethernet port :  [port("0/0"),port("0/2")] (opcional))"""
 
 
 
-
-
-
 """ wc( disConected1, disConected2 ,assigned ip(red), Area , portDispositivo1(opcional), portDispositivo2(opcional)) """""" connection oRouterDer matters!! """
 wanConection = [
-                WanConection(routers[0], routers[1], ip(100,10,0,8,29),0),
-                WanConection(routers[1], routers[2], ip(100,10,0,0,29),0),      
-                WanConection(routers[1], routers[3], ip(200,100,30,0,30),0),      
+                WanConection(routers[0], routers[1], ip(192,168,80,0,29),0),
+                WanConection(routers[0], routers[2], ip(192,168,80,8,29),0),      
                 ]
 
 
 """ fc( disConected1, disConected2, Area ,portDispositivo1(opcional), portDispositivo2(opcionale)) """""" connection order matters!! """
 fastEthernetConection = [                
                 FastEthernetConection(hosts[0], routers[0],0),
-                FastEthernetConection(hosts[1], routers[0],0),
-                
-                FastEthernetConection(hosts[2], routers[2],0),
-                FastEthernetConection(hosts[3], routers[3],0),
-                
-
-                
+                FastEthernetConection(hosts[1], routers[1],0),
+     
+                FastEthernetConection(hosts[2], routers[0],0),
+                FastEthernetConection(hosts[3], routers[2],0),  
                 ]
 
 
@@ -121,3 +117,4 @@ print(Routing.addressingOSPF(routers))
 """ print(Routing.addresingStatic(routers,hosts)) """
 
 """ -------------------show running-config ------------------------- """
+""" -------------------show ip nat translations ------------------------- """
